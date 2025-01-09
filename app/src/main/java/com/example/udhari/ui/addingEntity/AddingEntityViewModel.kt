73aa41a979bfad95f31001404cff16a85927b614
Henil Chhipani <henilchhipani@gmail.com>
2025-01-10 00:47:15 +0530

    Making entity form

---
+++ app/src/main/java/com/example/udhari/ui/addingEntity/AddingEntityViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.udhari.ui.addingEntity
+
+import androidx.lifecycle.ViewModel
+import com.example.udhari.data.PreferenceDataStore
+import javax.inject.Inject
+
+class AddingEntityViewModel @Inject constructor(
+    private val dataStore: PreferenceDataStore
+) : ViewModel() {
+
+}

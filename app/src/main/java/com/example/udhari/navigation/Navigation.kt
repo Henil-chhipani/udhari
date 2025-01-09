ea15f0df0ca0521e717a95d94924de5d268d5148
Henil Chhipani <henilchhipani@gmail.com>
2025-01-10 02:20:09 +0530

    added navigation animation

--- app/src/main/java/com/example/udhari/navigation/Navigation.kt
+++ app/src/main/java/com/example/udhari/navigation/Navigation.kt
@@ -1,9 +1,15 @@
 package com.example.udhari.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.interaction.HoverInteraction
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -15,17 +21,56 @@ fun Navigation(navHostController: NavHostController) {
     val context = LocalContext.current
     CompositionLocalProvider(GlobalNavController provides navHostController) {
         NavHost(navController = navHostController, startDestination = "home") {
-            composable("home") {
+            composable(
+                route = "home",
+                enterTransition = ::slideIntoLeft,
+                exitTransition = ::slideOutLeft,
+                popEnterTransition = ::slideIntoRight,
+                popExitTransition = ::slideOutLeft
+            ) {
                 HomeScreen()
             }
-            composable("addEntity") {
+            composable(
+                route = "addEntity",
+                enterTransition = ::slideIntoLeft,
+                exitTransition = ::slideOutLeft,
+                popExitTransition = ::slideOutRight,
+                popEnterTransition = ::slideIntoLeft
+            ) {
                 AddEntityScreen()
             }
-
         }
     }
 }
 
 val GlobalNavController = staticCompositionLocalOf<NavHostController> {
     error("NavController not provided")
+}
+
+fun slideIntoLeft(direction: AnimatedContentTransitionScope<NavBackStackEntry>): EnterTransition {
+    return direction.slideIntoContainer(
+        AnimatedContentTransitionScope.SlideDirection.Left,
+        animationSpec = tween(700)
+    )
+}
+
+fun slideIntoRight(direction: AnimatedContentTransitionScope<NavBackStackEntry>): EnterTransition {
+    return direction.slideIntoContainer(
+        AnimatedContentTransitionScope.SlideDirection.Right,
+        animationSpec = tween(700)
+    )
+}
+
+fun slideOutLeft(direction: AnimatedContentTransitionScope<NavBackStackEntry>): ExitTransition {
+    return direction.slideOutOfContainer(
+        AnimatedContentTransitionScope.SlideDirection.Left,
+        animationSpec = tween(700)
+    )
+}
+
+fun slideOutRight(direction: AnimatedContentTransitionScope<NavBackStackEntry>): ExitTransition {
+    return direction.slideOutOfContainer(
+        AnimatedContentTransitionScope.SlideDirection.Right,
+        animationSpec = tween(700)
+    )
 }
